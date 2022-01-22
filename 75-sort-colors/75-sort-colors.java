6ab8dff0f3ef27ce5df4b985fdae226c0830066c
Ekaansh Sahni <ekaanshsahni@gmail.com>
2022-01-22 14:52:20 +0530

    Time: 0 ms (100.00%), Space: 37.9 MB (49.51%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                i--;
+                right--;
+            }
+        }
+        
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
