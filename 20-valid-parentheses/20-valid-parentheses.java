75408a943888c1f40d030b8b4e1bb3f50ce2a624
Ekaansh Sahni <ekaanshsahni@gmail.com>
2022-01-31 18:51:31 +0530

    Time: 2 ms (60.71%), Space: 42.6 MB (5.07%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch =='{'||ch=='('||ch=='[')
+            {
+                stack.push(ch);
+            }
+            else if(stack.isEmpty())
+            {
+                return false;
+            }
+            else if(ch=='}')
+            {
+                if(stack.peek()=='{') {
+                    stack.pop();
+                }
+                else
+                    return false;
+            }
+            else if(ch==']')
+            {
+                if(stack.peek()=='[') {
+                    stack.pop();
+                }
+                else
+                    return false;
+            }
+            else if(ch==')')
+            {
+                if(stack.peek()=='(') {
+                    stack.pop();
+                }
+                else
+                    return false;
+            }
+        }
+        if(!stack.isEmpty())
+            return false;
+        return true;
+    }
+}
